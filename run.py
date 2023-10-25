c288e87d3460133ba363ca745d3be19f4abbc433
Afjunior18 <>
2023-10-25 19:44:01 +0000

    Create clear_terminal function and import os

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from welcome_message import welcome_message
+from clear_terminal import clear_terminal
 
+# Welcome message and display player's name
 player_name = welcome_message()
 print(f"Let's have some fun...{player_name}\n")
+
+
+clear_terminal()
