1e303d1cc4cb832fac285a6e890da22bf4323380
Afjunior18 <>
2023-10-26 20:46:44 +0000

    Implement input validation using try-except and loop for player in welcome message

--- blackj/welcome_message.py
+++ blackj/welcome_message.py
@@ -1,4 +1,18 @@
 def welcome_message():
-    print("Welcome to the Blackjack Game!\n")
-    name = input("What is your player's name?\n")
+    print("Blackjack Game!\n")
+    try:
+        name = input("What is your player's name?\n")
+        name = name.capitalize()
+        if not name.isalpha():
+            raise ValueError("Invalid input, Please enter letters only\n")
+    except ValueError as e:
+        print(e)
+        while True:
+            name = input("Please enter your name, 'letters only'\n")
+            name = name.capitalize()
+            if name.isalpha():
+                break
+    else:
+        print(f"Welcome {name} to the Blackjack Game!\nLet's have fun...\n")
+
     return name
--- run.py
+++ run.py
@@ -3,7 +3,10 @@ from blackj.clear_terminal import clear_terminal
 
 # Welcome message and display player's name
 player_name = welcome_message()
-print(f"Let's have some fun...{player_name}\n")
+
+
 
 
 if __name__ == "__main__":
+    
+    welcome_message()
