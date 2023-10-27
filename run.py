cda9578cea543f08d8f3f961d6a3bf1ade727d6f
Afjunior18 <>
2023-10-27 19:53:27 +0000

    Create rules_blackjack function for displaying game rules

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from blackj.welcome_message import welcome_message
 from blackj.clear_terminal import clear_terminal
+from blackj.rules_blackjack import rules_blackjack
 
 # Welcome message and display player's name
 player_name = welcome_message()
@@ -9,4 +10,5 @@ player_name = welcome_message()
 
 if __name__ == "__main__":
     
-    welcome_message()
+    
+    rules_blackjack()
