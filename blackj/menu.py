49aaf76203e6da1b493f8321f7392085b7fcae02
Afjunior18 <>
2023-10-27 20:46:22 +0000

    Add while loop to menu function, assuring validate input

--- blackj/menu.py
+++ blackj/menu.py
@@ -1,15 +1,18 @@
 from blackj.rules_blackjack import rules_blackjack
 
 def menu():
-    print("Wanna see the GAME RULES, press (1) or START GAME preesing (2)\n")
-    start_game = 2
-    rules_game = 1
+    while True:
+        print("Wanna see the GAME RULES, press (1) or START GAME preesing (2)\n")
+        start_game = 2
+        rules_game = 1
     
-    choice = input("Enter your choice: ")
+        choice = input("Enter your choice: ")
     
-    if choice == "1":
-        rules_blackjack()
-    elif choice == "2":
-        pass 
-    else:
-        print("Invalid Choice: Please choose (1) or (2)")
+        if choice == "1":
+            rules_blackjack()
+            break
+        elif choice == "2":
+            pass
+            break
+        else:
+            print("Invalid Choice: Please choose '1' or '2'")
