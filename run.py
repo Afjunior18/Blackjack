1ad14757800c72e1bb572dd52543240db34861a0
Afjunior18 <>
2023-10-27 20:32:40 +0000

    Create function menu, give user option to game rules or start game

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from blackj.welcome_message import welcome_message
 from blackj.clear_terminal import clear_terminal
 from blackj.rules_blackjack import rules_blackjack
+from blackj.menu import menu
 
 # Welcome message and display player's name
 player_name = welcome_message()
@@ -8,7 +9,8 @@ player_name = welcome_message()
 
 
 
+
 if __name__ == "__main__":
     
+    menu()
     
-    rules_blackjack()
