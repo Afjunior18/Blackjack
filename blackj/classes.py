859e64f348b7b2e93241a4a73b167c89dac36b1e
Afjunior18 <>
2023-10-28 10:51:57 +0000

    Implement card and deck classes with display function

---
+++ blackj/classes.py
@@ -0,0 +1,22 @@
+class Card:
+    """
+    Class that represents one card
+    """
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit 
+
+
+class Deck:
+    """
+    Class that represents all thes cards in a deck.
+    Will display
+    """
+    def __init__(self):
+        suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+    
+    def display_deck(self):
+        for card in self.cards:
+            print(f"{card.value} of {card.suit}")
--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from blackj.welcome_message import welcome_message
 from blackj.clear_terminal import clear_terminal
 from blackj.rules_blackjack import rules_blackjack
 from blackj.menu import menu
-
-# Welcome message and display player's name
-player_name = welcome_message()
-
+from blackj.classes import Card
+from blackj.classes import Deck
 
 
+my_deck = Deck()
 
 
 if __name__ == "__main__":
     
+    welcome_message()
     menu()
     
+    my_deck.display_deck()
